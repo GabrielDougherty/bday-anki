a5cd0e267406dc0dc00794f5ecda4bf1fd6dbae6
Gabriel Dougherty <contact@gabrieldougherty.com>
2025-08-07 13:32:47 -0400

    Add release infra (#2)

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+echo -n 'What version? '
+read VERSION
+
+echo "got version $VERSION"
+sed -i '' "s/<string>1\.0<\/string>/<string>$VERSION<\/string>/g" AnkiBirthdays.app/Contents/Info.plist
+exit
+zig build
+cp zig-out/bin/bdays AnkiBirthdays.app/MacOS
+create-dmg \
+    --volname "Anki Birthdays Installer" \
+    --window-pos 200 120 \
+    --window-size 600 300 \
+    --icon-size 100 \
+    --icon "AnkiBirthdays.app" 175 120 \
+    --hide-extension "AnkiBirthdays.app" \
+    --app-drop-link 425 120 \
+    "AnkiBirthdays-$VERSION.dmg" \
+    "AnkiBirthdays.app"
+
