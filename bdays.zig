07e214c583935ba4ab4c859b026697f2c6d0ba53
Gabriel Dougherty <contact@gabrieldougherty.com>
2025-08-06 22:01:20 -0400

    progress

--- bdays.zig
+++ bdays.zig
@@ -8,25 +8,13 @@ pub fn main() !void {
     child.stdout_behavior = .Pipe;
     try child.spawn();
 
-    const exit_code = child.wait();
-    if (exit_code == 0) {
-        const stdout = child.stdout;
+    // Read stdout before waiting
+    const stdout_data = try child.stdout.?.readToEndAlloc(arena.allocator(), 1024);
 
-        // create the buffered reader that will read from our file
-        var br = std.io.bufferedReaderSize(64, stdout.reader());
-
-        // set up our destination buffer
-        var dest_buf: [br.buf.len]u8 = undefined;
-
-        // wrap our destination buffer with a stream interface
-        var fbs = std.io.fixedBufferStream(&dest_buf);
-
-        // read our data
-        try br.reader().streamUntilDelimiter(fbs.writer(), '\n', br.buf.len);
-
-        // print
-        std.debug.print("{s}\n", .{fbs.getWritten()});
+    const exit_code = try child.wait();
+    if (exit_code == .Exited and exit_code.Exited == 0) {
+        std.debug.print("{s}", .{stdout_data});
     } else {
-        std.debug.print("Got an error");
+        std.log.info("Got an error", .{});
     }
 }
