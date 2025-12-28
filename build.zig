bf9b865cd7e83aa95c27041c493b88736ab53c59
Gabriel Dougherty <contact@gabrieldougherty.com>
2025-12-28 17:55:38 -0500

    Remove all nix stuff since it's broken

--- build.zig
+++ build.zig
@@ -14,23 +14,7 @@ pub fn build(b: *std.Build) void {
         }),
     });
 
-    // Check if we're in a nix environment
-    const is_nix = b.graph.env_map.get("IN_NIX_SHELL") != null;
-    if (is_nix) {
-        // In nix, use the host system frameworks (impure build)
-        std.debug.print("Building in nix environment - using host system frameworks\n", .{});
-
-        // Add system framework search paths
-        exe.root_module.addFrameworkPath(b.path("/System/Library/Frameworks"));
-        exe.root_module.addFrameworkPath(b.path("/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks"));
-
-        // Don't link objc explicitly - it's provided by the frameworks on modern macOS
-        std.debug.print("Added system framework paths (objc provided by frameworks)\n", .{});
-    } else {
-        // Normal system build
-        exe.linkSystemLibrary("objc");
-    }
-
+    exe.linkSystemLibrary("objc");
     exe.linkFramework("Cocoa");
     exe.linkFramework("Foundation");
 
